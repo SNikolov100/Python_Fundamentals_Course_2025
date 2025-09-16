3f126ee165378e22d489cf4ed5761406aa92fe9b
Svetoslav Nikolov <snikolov100@gmail.com>
2025-09-16 14:05:44 +0300

    Update 01_find_the_largest.py

--- 01_More_Exercise_Basic_Syntax_Conditional_Statements_and_Loops/01_find_the_largest.py
+++ 01_More_Exercise_Basic_Syntax_Conditional_Statements_and_Loops/01_find_the_largest.py
@@ -1,8 +1,11 @@
 enter_number = input()
-new_number = ""
-count = len(str(enter_number))
 
-for index in range(count-1, -1, -1):
-    new_number += enter_number[index]
+# new_number = ""
+# count = len(str(enter_number))
+#
+# for index in range(count-1, -1, -1):
+#     new_number += enter_number[index]
+
+new_number = int(str(enter_number)[::-1])
 
 print(new_number)
